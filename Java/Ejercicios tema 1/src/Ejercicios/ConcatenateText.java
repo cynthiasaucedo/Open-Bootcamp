78750673c2e1bb0500c47b9b88ab22166784fec1
Cynthia Saucedo <cynthiasaucedo97@gmail.com>
2022-09-16 11:00:23 -0300

    exercise 3

---
+++ Java/Ejercicios tema 1/src/Ejercicios/ConcatenateText.java
@@ -0,0 +1,14 @@
+//Crear un bucle que permita concatenar textos e imprima el resultado
+//separados por un espacio
+//método concat o +
+
+package Ejercicios;
+
+public class ConcatenateText {
+    public static void main(String[] args){
+        String[] names = {"Maria", "Leonardo", "Carolina", "Victoria"};
+                for(int i = 0; i < names.length; i++){
+                    System.out.println(names[i] + " ");
+                }
+    }
+}
